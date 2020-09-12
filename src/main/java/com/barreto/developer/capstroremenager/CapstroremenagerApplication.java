c32ef36de212ca39791c96c538cd2b082e595b3b
rael5000 <barreto.israel5000@gmail.com>
2020-09-11 23:41:19 -0300

    First Commit: project structure created

---
+++ src/main/java/com/barreto/developer/capstroremenager/CapstroremenagerApplication.java
@@ -0,0 +1,13 @@
+package com.barreto.developer.capstroremenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstroremenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstroremenagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barreto/developer/capstroremenager/CapstroremenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barreto.developer.capstroremenager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstroremenagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
