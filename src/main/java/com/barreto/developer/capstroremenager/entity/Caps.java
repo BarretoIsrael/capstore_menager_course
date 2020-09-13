8aa902fe43da498897f242c9f66cbabb534eb995
rael5000 <barreto.israel5000@gmail.com>
2020-09-13 20:49:14 -0300

    Lombok Anotations added in classes

--- src/main/java/com/barreto/developer/capstroremenager/entity/Caps.java
+++ src/main/java/com/barreto/developer/capstroremenager/entity/Caps.java
@@ -1,8 +1,18 @@
 package com.barreto.developer.capstroremenager.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Caps {
 
     @Id
